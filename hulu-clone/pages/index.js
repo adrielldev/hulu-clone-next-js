a39b0fceb50468b1a70582b1a5bddec2ff33c4f3
adriel <adrieldev@outlook.com>
2023-01-18 13:51:51 -0300

    feat-nav

--- hulu-clone/pages/index.js
+++ hulu-clone/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../Components/Header'
+import Nav from '../Components/Nav'
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
      <Header/>
+     <Nav/>
      
     </>
   )
