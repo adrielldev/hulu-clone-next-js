2b99bc6af1bfb56ed5c44d6bb6b6791daa2e0cd5
adriel <adrieldev@outlook.com>
2023-01-18 13:10:17 -0300

    feat-header

--- hulu-clone/next.config.js
+++ hulu-clone/next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images:{
+    domains:[
+      "links.papareact.com",
+      "image.tmdb.org"
+    ]
+    
+  }
+}
